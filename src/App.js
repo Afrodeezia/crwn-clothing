1b18b27e0452016b28594c1e6941d80723991d78
Marco Roldan <marcopauloangeloroldan1@gmail.com>
2022-09-22 16:52:52 +0800

    refactored the code to andrei's approach. Nested Route is working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
+
+import {Routes, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import HatsPage from './pages/homepage/hatspage.component';
 
 
-import { Outlet } from 'react-router-dom';
+
 
 import './App.css';
 
 function App() {
   return (
     <div>
-      <HomePage />
-      <Outlet />
+      <Routes>
+        <Route path='/' element={<HomePage />} >
+          <Route path='/hats' element={<HatsPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.js';
-import HatsPage from './pages/homepage/hatspage.component';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
 
-      <Routes>
-        <Route path='/' element={<App />} >
-          <Route path='/hats' element={<HatsPage />} />
-        </Route>
-      </Routes>
+      <App />
 
       </BrowserRouter>
   </React.StrictMode>
