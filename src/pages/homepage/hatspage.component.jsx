4994a0296c41d073154fc2ba764509884130ee5c
Marco Roldan <marcopauloangeloroldan1@gmail.com>
2022-09-22 15:54:05 +0800

    currently routing but having problems in nested routing.

---
+++ src/pages/homepage/hatspage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Outlet } from "react-router-dom";
+
+const HatsPage = () => (
+    <div>
+      <h1>HATS PAGE</h1>
+      <Outlet />
+      </div>
+   
+  );
+
+  export default HatsPage;
