56a3ede18751b79286f356d09ed02f34b520f2c2
Marco Roldan <marcopauloangeloroldan1@gmail.com>
2022-09-27 21:09:13 +0800

    got immersed in forms but need improvement more on learning. next stop firebase

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx'
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component'
 
 
 
@@ -18,6 +19,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
+        <Route path='/signIn' element={<SignInAndSignUpPage />} />
       </Routes>
     </BrowserRouter>
 
